f9631e85e1b2747b8080c10ae05b513ea4306d32
Ciberth <thomasclauwaert@gmail.com>
2018-05-09 15:37:10 +0200

    updates

--- reactive/consumer-app.py
+++ reactive/consumer-app.py
@@ -8,24 +8,38 @@ from charmhelpers.core.hookenv import log, status_set, config
 from charmhelpers.core.templating import render
 from charms.reactive import when, when_not, set_flag, clear_flag, when_file_changed
 
+@when('apache.available')
+def finishing_up_setting_up_sites():
+    host.service_reload('apache2')
+    set_flag('apache.start')
+
+@when('apache.start')
+def ready():
+    host.service_reload('apache2')
+    status_set('active', 'apache ready')
+
 # todo here @when_not('generic-database.concrete')
 # for when a new consumer app connects to a generic database that is not generic anymore
-@when('generic-database.joined')
+@when('endpoint.generic-database.joined')
 def request_db():
-    endpoint = endpoint_from_flag('gneric-database.joined')
+    endpoint = endpoint_from_flag('endpoint.generic-database.joined')
     endpoint.request('postgresql')
     status_set('maintenance', 'Requesting postgresql')
 
+@when('apache.start')
+@when_not('endpoint.generic-database.connected')
+def waiting_for_db():
+    status_set('maintenance', 'Waiting for generic database relation')
 
 
-@when('generic-database.pgsql.available')
+@when('endpoint.generic-database.postgresql.available')
 def pgsql_render_config():
     """
     The generic-database represents a pgsql database. This function will render the config file in order to use the pgsql database.
     Configuration details: host(), port(), databasename(), user(), password() 
     """
     
-    pgsql = endpoint_from_flag('generic-database.pgsql.available')
+    pgsql = endpoint_from_flag('endpoint.generic-database.postgresql.available')
 
     render_template('pgsql-config.j2', '/var/consumer-app/test.html', {
         'gdb_host' : pgsql.host(),
@@ -34,6 +48,7 @@ def pgsql_render_config():
         'gdb_user' : pgsql.user(),
         'gdb_password' : pgsql.password(),
     })
+    set_flag('endpoint.generic-database.connected')
     set_flag('restart-app')
 
 
