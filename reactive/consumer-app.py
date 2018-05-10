fe05a934a32c11b6cc69731aad87b8fd7e7b9bac
Ciberth <thomasclauwaert@gmail.com>
2018-05-10 15:18:15 +0200

    minor updates

--- reactive/consumer-app.py
+++ reactive/consumer-app.py
@@ -9,29 +9,44 @@ from charmhelpers.core.templating import render
 from charms.reactive import when, when_not, set_flag, clear_flag, when_file_changed, endpoint_from_flag
 from charms.reactive import Endpoint
 
+###########################################################################
+#                                                                         #
+# Installation of apache + waiting for generic-database (provider) charm  #
+#                                                                         #
+###########################################################################
+
 @when('apache.available')
 def finishing_up_setting_up_sites():
     host.service_reload('apache2')
     set_flag('apache.start')
-# todo deze in commentaar - waiting for db vangt dit op
-#@when('apache.start')
-#def ready():
-#    host.service_reload('apache2')
-#    status_set('active', 'apache ready')
 
-# todo here @when_not('generic-database.concrete')
-# for when a new consumer app connects to a generic database that is not generic anymore
-@when('endpoint.generic-database.joined')
-def request_db():
-    endpoint = endpoint_from_flag('endpoint.generic-database.joined')
-    endpoint.request('postgresql')
-    status_set('maintenance', 'Requesting postgresql')
 
 @when('apache.start')
 @when_not('endpoint.generic-database.connected')
 def waiting_for_db():
     host.service_reload('apache2')
     status_set('maintenance', 'Waiting for generic database relation')
+
+
+########################################################################
+#                                                                      #
+# Request of database technology to generic-database (provider charm)  #
+#                                                                      #
+########################################################################
+
+
+@when('endpoint.generic-database.joined')
+def request_db():
+    endpoint = endpoint_from_flag('endpoint.generic-database.joined')
+    endpoint.request('postgresql')
+    status_set('maintenance', 'Requesting postgresql')
+
+
+##################################################
+#                                                #
+# Request successful, get data and render config # 
+#                                                #
+##################################################
 
 
 @when('endpoint.generic-database.postgresql.available')
